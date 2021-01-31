67aa381fffbc3d61eef156919dc3073f5b261a9d
James StaAna <jamesmartin.staana@gmail.com>
2021-01-31 14:33:13 +0800

    movement_update

--- HomeAgainGame/Assets/Scripts/May_control_script.cs
+++ HomeAgainGame/Assets/Scripts/May_control_script.cs
@@ -15,7 +15,21 @@ public class May_control_script : MonoBehaviour
 
     private void Update()
     {
+        // Character Movement:
         var movement = Input.GetAxis("Horizontal");
+
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = -6;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = 6;
+        }
+        transform.localScale = characterScale;
+
+        // Character Animation 
         animator.SetFloat("speed", Mathf.Abs(movement));
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
     }
