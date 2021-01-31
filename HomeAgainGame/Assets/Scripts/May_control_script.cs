8d4f483b11969158ca29085a84b3c39659c3e348
James StaAna <jamesmartin.staana@gmail.com>
2021-01-31 14:18:53 +0800

    movement

---
+++ HomeAgainGame/Assets/Scripts/May_control_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class May_control_script : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    public Animator animator;
+
+    private void Start()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(movement));
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+    }
+}
