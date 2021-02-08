f44c1c7a1322d7cd598d8ca3c023277acfcaf782
James StaAna <jamesmartin.staana@gmail.com>
2021-02-08 22:45:40 +0800

    Update_on_functions

---
+++ HomeAgainGame/Assets/Scripts/Object_Opacity_pass_thru.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class Object_Opacity_pass_thru : MonoBehaviour
+{
+    float alphaLevel;
+    SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+            alphaLevel = .5f;
+
+        sprite.color = new Color(1, 1, 1, alphaLevel);
+
+
+        Debug.Log(collision.name + "Enter");
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+            alphaLevel = 1f;
+
+        sprite.color = new Color(1, 1, 1, alphaLevel);
+    }
+ 
+}
---
+++ HomeAgainGame/Assets/Scripts/Object_Sprite_Change_cause.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Sprite_Change_cause : MonoBehaviour
+{
+    public Sprite active;
+    public Sprite inactive;
+    private SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = inactive;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindWithTag("May_room_light") != null)
+        {
+            sr.sprite = active;
+        }
+        else
+        {
+            sr.sprite = inactive;
+
+        }
+       
+    }
+}
