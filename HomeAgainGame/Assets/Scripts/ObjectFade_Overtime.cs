2f6b4756dbfb4f56d186c82ff44e2b9585682af8
James StaAna <jamesmartin.staana@gmail.com>
2021-02-28 22:06:57 +0800

    Scene Updates

---
+++ HomeAgainGame/Assets/Scripts/ObjectFade_Overtime.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFade_Overtime : MonoBehaviour
+{
+
+    public GameObject End;
+    public GameObject ObjectFade;
+    Animator m_Animator;
+    bool isfading = false;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        m_Animator = ObjectFade.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (isfading == true)
+        {
+            m_Animator.SetTrigger("fade");
+
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isfading = true;
+         
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            End.SetActive(true);
+
+        }
+    }
+
+
+
+
+}
---
+++ HomeAgainGame/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransition : MonoBehaviour
+{
+    public GameObject nextScene;
+    public float currentTime = 0f;
+    public float startTime = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+
+        if (this.gameObject != null && currentTime == 0f)
+        {
+            Destroy(gameObject);
+            currentTime = 2f;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        nextScene.SetActive(true);
+    }
+}
