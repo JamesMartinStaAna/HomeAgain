6ffa1e04c7ac8763fc6e722016876371bfbdec66
James StaAna <jamesmartin.staana@gmail.com>
2021-05-13 03:47:57 +0800

    skip update

---
+++ HomeAgainGame/Assets/Scripts/SkipCutscene2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipCutscene2 : MonoBehaviour
+{
+    public GameObject skip1;
+    public GameObject skip2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void Skip()
+    {
+        Destroy(skip1);
+        Destroy(skip2);
+    }
+}
