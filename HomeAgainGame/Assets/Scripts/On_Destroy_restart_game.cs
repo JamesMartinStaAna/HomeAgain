fa5a305c0df4ee3adfc6acce767171cb399ca848
James StaAna <jamesmartin.staana@gmail.com>
2021-05-19 20:10:02 +0800

    building webgl

---
+++ HomeAgainGame/Assets/Scripts/On_Destroy_restart_game.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class On_Destroy_restart_game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
