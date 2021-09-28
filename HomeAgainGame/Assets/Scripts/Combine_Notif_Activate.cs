5835b399999b998f25b5432c5d1a4498db118b48
James StaAna <jamesmartin.staana@gmail.com>
2021-09-28 09:39:00 +0800

    Deleted unused code

---
+++ HomeAgainGame/Assets/Scripts/Combine_Notif_Activate.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combine_Notif_Activate : MonoBehaviour
+{
+
+
+    
+    public GameObject ActivateNotif;
+    public GameObject Deposit;
+    public string ObjectToBeChecked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // when cardboard box is stored activate boxview
+        if (ActivateNotif != null)
+        {
+            if (GameObject.FindWithTag(ObjectToBeChecked) != null)
+            {
+                ActivateNotif.SetActive(true);
+            }
+        }
+   
+
+ 
+        
+
+
+
+
+
+
+
+    }
+}
