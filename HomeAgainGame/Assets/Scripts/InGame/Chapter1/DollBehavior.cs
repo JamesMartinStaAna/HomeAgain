d49b61225825537e824bb8d46616cb416d7098dc
James StaAna <jamesmartin.staana@gmail.com>
2021-10-12 08:14:00 +0800

    Updates on chapter 1 - Silver key pickup

---
+++ HomeAgainGame/Assets/Scripts/InGame/Chapter1/DollBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DollBehavior : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+
+    }
+
+    private void OnDestroy()
+    {
+        if (GameObject.FindWithTag("doll_receiver") == null)
+        {
+            SoundManager.PlaySound("popUp");
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    //item can be added to inventory
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    break;
+
+                }
+            }
+
+
+
+        }
+
+    }
+}
