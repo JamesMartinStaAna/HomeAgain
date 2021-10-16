6653e1e3d44cff5453345f964de529adb695fb94
James StaAna <jamesmartin.staana@gmail.com>
2021-10-16 22:37:32 +0800

    ObjectInteract spriteswap added

---
+++ HomeAgainGame/Assets/Scripts/UI/ObjectInteract_SpriteSwap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteract_SpriteSwap : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite ActiveSprite;
+    public Sprite InactiveSprite;
+
+    private void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        spriteRenderer.sprite = ActiveSprite;
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        spriteRenderer.sprite = InactiveSprite;
+    }
+}
